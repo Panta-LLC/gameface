ddd513df34e71196ad2fe4061b389ff513a7d613
Damon Hastings <damonjhastings@gmail.com>
2025-11-15 09:19:03 -0800

    Refactor code structure for improved readability and maintainability

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { ThemeProvider } from './components/theme-provider';
 
 const root = createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
